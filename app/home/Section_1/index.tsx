65d480fcd39f2b332e1c78673117ff42ddb7cadc
luchaoet <luchaoet@icloud.com>
2024-03-27 19:20:39 +0800

    update

--- app/home/Section_1/index.tsx
+++ app/home/Section_1/index.tsx
@@ -9,9 +9,9 @@ function App() {
         <dl className='g-c-w g-d-f g-fd-c'>
           <dt className='g-fw-b g-fs-0'>Large Stock Alloy Wheel<br />Ready to Ship</dt>
           <dd>Wheelshome has over 30,000 pcs wheels in stock, most orders less than a container can be sent 3 to 7 days. We have most car models replica wheels, after market wheels and forged alloy wheels, you can request our stock list to choose your wheels by click below button</dd>
-          <dd className={styles.button}>
+          {/* <dd className={styles.button}>
             <Button size="large" type="primary">Check Our Stock List</Button>
-          </dd>
+          </dd> */}
         </dl>
         <div className="g-fg-1 g-mw-0 g-w-100per">
           <Carousel autoplay>
